926895616e492e90db99a99d92655a6c3660ed49
Daniel Guevara <a.daniel.g@hotmail.com>
2018-06-11 01:33:03 -0500

    Mi actualizacion

---
+++ WadProject-master-2/WadProjectLogin/src/java/com/wad/bean/UsuarioBean.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wad.bean;
+ 
+import com.wad.dao.TipoUsuarioDAO;
+import com.wad.dao.UsuarioDAO;
+import com.wad.model.TipoUsuario;
+import com.wad.model.Usuario;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author danielguevara
+ */
+@ManagedBean(name = "usuarioBean")
+@SessionScoped
+public class UsuarioBean implements Serializable {
+
+    private Integer idUsuario;
+    private String idTipo;
+    private TipoUsuario tipoUsuario;
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String username;
+    private String password;
+    
+    public UsuarioBean() {
+        
+    }
+    
+    public String addUsuario(){
+        Usuario usuario= new Usuario(getNombre(), getPrimerApellido(), getSegundoApellido(), getUsername(), getPassword());
+        UsuarioDAO usuarioDAO= new UsuarioDAO();
+        usuarioDAO.addUsuario(usuario);
+        return "iniciarSesion";
+    }
+    
+    public String loginUsuario(){
+        Usuario usuario= new Usuario(getUsername(), getPassword());
+        UsuarioDAO usuarioDAO= new UsuarioDAO();
+        Usuario aux = usuarioDAO.loginUsuarioDAO(usuario);
+        if(aux!=null){
+            HttpSession s = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+            s.setAttribute("sesionusuario", aux.getUsername());
+            TipoUsuario tipo = aux.getTipoUsuario();
+            //Administrador =1, Alumno = 2, Profesor = 3
+            if(tipo.getIdTipo()==1){
+                return "bienvenido";
+            }
+            if(tipo.getIdTipo()==2){
+                return "bienvenidoAlumno";
+            }
+            if(tipo.getIdTipo()==3){
+                return "bienvenidoProfesor";
+            }  
+        }
+        else
+            return "index";
+        return "index";
+    }
+    
+    public void addUsuarioPrueba(){
+        TipoUsuario tipo = new TipoUsuario(Integer.parseInt(idTipo));   
+        this.tipoUsuario=tipo;
+        Usuario usuario= new Usuario(getTipoUsuario(), getNombre(), getPrimerApellido(), getSegundoApellido(), getUsername(), getPassword());
+        UsuarioDAO usuarioDAO= new UsuarioDAO();
+        usuarioDAO.addUsuario(usuario);
+    }
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(String idTipo) {
+        this.idTipo = idTipo;
+    }
+    
+}
